aa48038c258aa8623601a6f278d32e1b4001ac0f
David-Whiteford <c00204740@itcarlow.ie>
2020-10-04 17:05:16 +0100

    The completed Filter for Tester

---
+++ Assets/Tests/CubeFilterTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class CubeFilterTest
+    {
+        
+        [Test]
+        public void CubeFilterSimple()
+        {
+            int[] input = { 1, 3, 5, 7, 9, 999 };
+            int[] output = CubeFilter.cubes(input);
+            int[] expected = { 1, 27, 125, 343, 729, 997002999 };
+
+            foreach (var o in output)
+            {
+                Debug.Log(o);
+            }
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+
+
+    public class ModuloFirstTest
+    {
+
+        [Test]
+        public void ModuloFirstSimple()
+        {
+            int[] input = { 4, 2, 8, 3, 9, 4, 10, 5, 11, 6 };
+            int[] output = ModuloFirst.ModFirstVals(input);
+            int[] expected = { 0, 2, 0, 3, 1, 0, 2, 1, 3, 2 };
+
+            foreach (var o in output)
+            {
+                Debug.Log(o);
+            }
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+
+    }
+}
+
+
