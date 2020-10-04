7dfadaeb780ddc8cfd1450b03a97f37c2d2cd714
David-Whiteford <c00204740@itcarlow.ie>
2020-10-04 17:22:38 +0100

    Added a test for the minus

--- Assets/Tests/CubeFilterTest.cs
+++ Assets/Tests/CubeFilterTest.cs
@@ -23,26 +23,7 @@ namespace Tests
     }
 
 
-    public class ModuloFirstTest
-    {
-
-        [Test]
-        public void ModuloFirstSimple()
-        {
-            int[] input = { 4, 2, 8, 3, 9, 4, 10, 5, 11, 6 };
-            int[] output = ModuloFirst.ModFirstVals(input);
-            int[] expected = { 0, 2, 0, 3, 1, 0, 2, 1, 3, 2 };
-
-            foreach (var o in output)
-            {
-                Debug.Log(o);
-            }
-
-            CollectionAssert.AreEqual(expected, output);
-        }
-
-
-    }
+  
 }
 
 
